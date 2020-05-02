3394555e5c313534a02c662524c4afbb10e75baf
desbiensa <desbiensa1@gmail.com>
2020-05-01 22:29:29 -0400

    git ingnore file

---
+++ speedReport.py
@@ -0,0 +1,23 @@
+import pandas
+import matplotlib.pyplot as plt
+from pandas.plotting import parallel_coordinates
+from pandas import DataFrame
+import csv
+
+x = []
+y = []
+
+#IMPORT CSV
+with open("C:\Program Files\ookla-speedtest-1.0.0-win64\speed.csv", 'r', header=False) as csvfile:
+    plots = csv.reader(csvfile, delimiter="  ")
+    for row in plots:
+        x.append(int(row[0]))
+        y.append(int(row[1]))
+
+plt.plot(x,y, marker='o')
+plt.title('Bandwidth Report - To this day')
+plt.xlabel('Mbps')
+plt.ylabel('Date')
+
+plt.show()
+    
